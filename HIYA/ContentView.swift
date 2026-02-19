781dede0fff4f43c2a23c9ff3f2fae05f9a69ae1
Guillaume Richard <guitch95@gmail.com>
2026-02-19 09:14:31 +0100

    Created the MainView with chech availibility of Apple Intelligence

--- HIYA/ContentView.swift
+++ HIYA/ContentView.swift
@@ -9,12 +9,22 @@ import SwiftUI
 import FoundationModels
 
 struct ContentView: View {
+    private var largeLanguageModel = SystemLanguageModel.default
+    @State private var response = ""
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            switch largeLanguageModel.availability {
+            case .available:
+                Text(response)
+            case .unavailable(.deviceNotEligible):
+                Text("Your device is not eligible for Apple Intelligence.")
+            case .unavailable(.appleIntelligenceNotEnabled):
+                Text("Please enable Apple Intelligence in Settings")
+            case .unavailable(.modelNotReady):
+                Text("The AI model is not ready.")
+            case .unavailable(_):
+                Text("The AI feature is unavailable for an unknown reason.")
+            }
         }
         .padding()
     }
