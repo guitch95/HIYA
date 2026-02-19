7fa38fabca33af4bccddd4089899c43652e08336
Guillaume Richard <guitch95@gmail.com>
2026-02-19 09:00:33 +0100

    Initial Commit

---
+++ HIYA/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  HIYA
+//
+//  Created by Guillaume Richard on 19/02/2026.
+//
+
+import SwiftUI
+import FoundationModels
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HIYA/HIYAApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIYAApp.swift
+//  HIYA
+//
+//  Created by Guillaume Richard on 19/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct HIYAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
